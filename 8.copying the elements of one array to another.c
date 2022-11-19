cce25471530acb98e36b28bf0f6c6536fc9317f1
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-11-20 02:02:53 +0530

    Create 8.copying the elements of one array to another.c

---
+++ 8.copying the elements of one array to another.c
@@ -0,0 +1,29 @@
+//my reg no is RA22110420100032
+#include <stdio.h>
+void main()
+{
+    int arr1[100], arr2[100];
+    int i, n;
+    printf("Input the number of elements to be stored in the array :");
+    scanf("%d",&n);
+    printf("Input %d elements in the array :\n",n);
+    for(i=0;i<n;i++)
+    {
+        printf("element - %d : ",i);
+	    scanf("%d",&arr1[i]);
+  	}
+    for(i=0; i<n; i++)
+    {
+        arr2[i] = arr1[i];
+    }
+    printf("\nThe elements stored in the first array are :\n");
+    for(i=0; i<n; i++)
+    {
+        printf("% 5d", arr1[i]);
+    }
+    printf("\n\nThe elements copied into the second array are :\n");
+    for(i=0; i<n; i++)
+    {
+        printf("% 5d\n", arr2[i]);
+    }
+}
